ae9a3fc740794945419e18b7d333c9c7b21fc4d2
Andrew Chen <yongjhih@gmail.com>
2016-07-05 12:39:42 +0800

    Fix Parses.all not found

--- parses.js
+++ parses.js
@@ -1,38 +1,7 @@
-;(function (factory) {
-  var objectTypes = {
-    'function': true,
-    'object': true
-  };
-
-  function checkGlobal(value) {
-    return (value && value.Object === Object) ? value : null;
-  }
-
-  var freeExports = (objectTypes[typeof exports] && exports && !exports.nodeType) ? exports : null;
-  var freeModule = (objectTypes[typeof module] && module && !module.nodeType) ? module : null;
-  var freeGlobal = checkGlobal(freeExports && freeModule && typeof global === 'object' && global);
-  var freeSelf = checkGlobal(objectTypes[typeof self] && self);
-  var freeWindow = checkGlobal(objectTypes[typeof window] && window);
-  var moduleExports = (freeModule && freeModule.exports === freeExports) ? freeExports : null;
-  var thisGlobal = checkGlobal(objectTypes[typeof this] && this);
-  var root = freeGlobal || ((freeWindow !== (thisGlobal && thisGlobal.window)) && freeWindow) || freeSelf || thisGlobal || Function('return this')();
-
-  // Because of build optimizers
-  if (typeof define === 'function' && define.amd) {
-    define(function (exports) {
-      return factory(root, exports);
-    });
-  } else if (typeof module === 'object' && module && module.exports === freeExports) {
-    module.exports = factory(root, module.exports);
-  } else {
-    root.Parses = factory(root, {});
-  }
-}.call(this, function (root, exp, undefined) {
+module.exports = Parses;
 
 function Parses() {
 }
-
-root.Parses = exp.Parses = Parses;
 
 var Parse = require('parse/node').Parse;
 var Rx = require('rx');
@@ -45,7 +14,7 @@ require('./rx.distincts');
 function all(query) {
   return allDesc(query);
 }
-Parses.prototype.all = all;
+Parses.all = all;
 
 function allAsc(query) {
   var chunkSize = 100;
@@ -61,7 +30,7 @@ function allAsc(query) {
     return it.id;
   });
 }
-Parses.prototype.allAsc = allAsc;
+Parses.allAsc = allAsc;
 
 function allDesc(query) {
   var chunkSize = 100;
@@ -77,7 +46,7 @@ function allDesc(query) {
     return it.id;
   });
 }
-Parses.prototype.allDesc = allDesc;
+Parses.allDesc = allDesc;
 
 /**
  * @param {Parse.Query} query
@@ -85,7 +54,7 @@ Parses.prototype.allDesc = allDesc;
 function find(query) {
   return Rx.Observable.fromPromise(query.find());
 }
-Parses.prototype.find = find;
+Parses.find = find;
 
 /**
  * @param {Parse.Query} query
@@ -94,7 +63,7 @@ Parses.prototype.find = find;
 function get(query, id) {
   return Rx.Observable.fromPromise(query.get(id));
 }
-Parses.prototype.get = get;
+Parses.get = get;
 
 /**
  * @param {Parse.Object} parseObject
@@ -104,7 +73,7 @@ function fetch(parseObject) {
     return parseObject;
   }).defaultIfEmpty(parseObject);
 }
-Parses.prototype.fetch = fetch;
+Parses.fetch = fetch;
 
 /**
  * @param {Parse.Object} parseObject
@@ -114,28 +83,28 @@ function save(parseObject) {
     return parseObject;
   }).defaultIfEmpty(parseObject);
 }
-Parses.prototype.save = save;
+Parses.save = save;
 
 function removeAll(parseQuery) {
   return all(parseQuery).concatMap(function (parseObject) {
     return parseObject.destroy({});
   });
 }
-Parses.prototype.removeAll = removeAll;
+Parses.removeAll = removeAll;
 
 function removeDup(parseQuery, keySelector) {
   return all(parseQuery).distincts(keySelector, null, function (it) {
     it.destroy({});
   });
 }
-Parses.prototype.removeDup = removeDup;
+Parses.removeDup = removeDup;
 
 function removeDupByColumn(parseQuery, column) {
   return removeDup(parseQuery, function (it) {
     return it.get(column);
   });
 }
-Parses.prototype.removeDupByColumn = removeDupByColumn;
+Parses.removeDupByColumn = removeDupByColumn;
 
 }));
 /* vim: set sw=2: */
