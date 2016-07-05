284a976a2b1e4b4c3cd2579b7fd1e7c92b7f223d
Andrew Chen <yongjhih@gmail.com>
2016-07-05 10:59:20 +0800

    Support global export for Parses

--- parses.js
+++ parses.js
@@ -1,10 +1,42 @@
-var Parse = require('parse/node').Parse;
-var Rx = require('rx');
-require('./rx.distincts');
-module.exports = Parses;
+;(function (factory) {
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+
+  function checkGlobal(value) {
+    return (value && value.Object === Object) ? value : null;
+  }
+
+  var freeExports = (objectTypes[typeof exports] && exports && !exports.nodeType) ? exports : null;
+  var freeModule = (objectTypes[typeof module] && module && !module.nodeType) ? module : null;
+  var freeGlobal = checkGlobal(freeExports && freeModule && typeof global === 'object' && global);
+  var freeSelf = checkGlobal(objectTypes[typeof self] && self);
+  var freeWindow = checkGlobal(objectTypes[typeof window] && window);
+  var moduleExports = (freeModule && freeModule.exports === freeExports) ? freeExports : null;
+  var thisGlobal = checkGlobal(objectTypes[typeof this] && this);
+  var root = freeGlobal || ((freeWindow !== (thisGlobal && thisGlobal.window)) && freeWindow) || freeSelf || thisGlobal || Function('return this')();
+
+  // Because of build optimizers
+  if (typeof define === 'function' && define.amd) {
+    define(function (exports) {
+      return factory(root, exports);
+    });
+  } else if (typeof module === 'object' && module && module.exports === freeExports) {
+    module.exports = factory(root, module.exports);
+  } else {
+    root.Parses = factory(root, {});
+  }
+}.call(this, function (root, exp, undefined) {
 
 function Parses() {
 }
+
+root.Parses = exp.Parses = Parses;
+
+var Parse = require('parse/node').Parse;
+var Rx = require('rx');
+require('./rx.distincts');
 
 /**
  * Required unset limit(), please use Rx.Observable.take() instead.
@@ -105,4 +137,5 @@ function removeDupByColumn(parseQuery, column) {
 }
 Parses.removeDupByColumn = removeDupByColumn;
 
+}));
 /* vim: set sw=2: */
