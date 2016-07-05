f3862a79565905dbd9107a9b7e6b3baf90c83ac1
Andrew Chen <yongjhih@gmail.com>
2016-07-05 11:11:43 +0800

    Add test

---
+++ test/test.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var expect = chai.expect;
+var nock = require('nock');
+var RxFacebook = require('../index');
+
+describe('parses', function() {
+  it('should be defined', function() {
+    expect(Parses).to.be.a('function');
+  });
+
+  it('should be .all defined', function() {
+    expect(Parses.all).to.be.a('function');
+  });
+
+  it('should be .find defined', function() {
+    expect(Parses.find).to.be.a('function');
+  });
+
+});
+
+/* vim: set sw=2: */
